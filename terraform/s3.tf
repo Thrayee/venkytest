e340fdc7b167f03691c3d6d409bb28092fcea6b8
Venky Susarla <Venky.Susarla@ness.com>
2023-03-27 16:01:27 -0500

    terraform.yml modified11

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,13 @@
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  description = "Bucket ACL"
+  type        =  string
+  default     = "private"
+}
+
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-bucket"
-  acl = "private"
+  acl = aws_s3_bucket_acl.my_bucket_acl.arn
 }
 
 output "s3_bucket_name" {
