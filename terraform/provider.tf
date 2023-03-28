edd9c7d63bbc7f4bfcba22cf1653f391f618e330
Venky Susarla <Venky.Susarla@ness.com>
2023-03-28 09:08:42 -0500

    terraform.yml modified24

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region  = var.aws_region
 
   # You can use access keys
-  # TF_VAR_AWS_ACCESS_KEY_ID = var.AWS_ACCESS_KEY_ID
-  # TF_VAR_AWS_SECRET_ACCESS_KEY = var.AWS_ACCESS_KEY_ID
+  ACCESS_KEY_ID = var.AWS_ACCESS_KEY_ID
+  AWS_SECRET_ACCESS_KEY = var.AWS_ACCESS_KEY_ID
 
   # Or specify an aws profile, instead.
   # profile = "<aws profile>"
